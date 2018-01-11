098b85639ea955a5d7eba574fa220621aad88658
Johnny-Zou <johnnyzou99@yahoo.ca>
2018-01-11 02:13:05 -0500

    added new images

---
+++ assets/js/header.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$(document).on("scroll",function(){
+		if($(window).scrollTop() === 0) {
+			$(".header .mainMenu").css("background-color", "rgba(0,0,0,0)");
+		}
+		else{
+			$(".header .mainMenu").css("background-color", "rgba(0,0,0,0.6)");
+		}
+		
+	})
+});

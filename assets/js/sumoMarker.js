0f4fba97172c2a507d9e379198fd526c99e54e3a
Johnny-Zou <johnnyzou99@yahoo.ca>
2018-09-12 23:22:25 -0400

    changed ART page

--- assets/js/sumoMarker.js
+++ assets/js/sumoMarker.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 
+	var hoverTimeOuts = [];
+
 	//Fader
-	$(".sumoRobot__imageFader").on("mouseenter", function(){
-		$(".sumoRobot__imageFader").css("opacity","0.2");
+	$(".robotMarker__imageFader").on("mouseenter", function(){
+		$(".robotMarker__imageFader").css("opacity","0.2");
 
 		//Function for changing the order the dots appear
 		(function($) {
@@ -21,67 +23,74 @@ $(document).ready(function(){
 			};
 		}(jQuery));
 
-		$(".sumoRobot__markerCircle").shuffle().each(function(index){
+		$(".robotMarker__markerCircle").shuffle().each(function(index){
 			var that = this;
-			setTimeout(function(){
-				$(that).css("opacity", "0.7");
-			}, 250*index);
-		});
-		// $(".sumoRobot__markerCircle").css("opacity", "0.7");
+			hoverTimeOuts.push(
+				setTimeout(function(){
+					$(that).css("opacity", "0.7");
+				}, 250*index)
+			);
 
+		});
+		// $(".robotMarker__markerCircle").css("opacity", "0.7");
 
 
 		
 	});
 
-	$(".sumoRobot__imageFader").on("mouseleave", function(){
-		if( $(".sumoRobot__markerCircle:hover").length !=0 ){
+	$(".robotMarker__imageFader").on("mouseleave", function(){
+		if( $(".robotMarker__markerCircle:hover").length !=0 ){
 			// console.log("hovering marker");
 		}
 		else{
-			$(".sumoRobot__imageFader").css("opacity","0");
-			$(".sumoRobot__markerCircle").css("opacity", "0");
+			// Stop future dots from appearing
+			for(var i =0; i<hoverTimeOuts.length; i++){
+				clearTimeout(hoverTimeOuts[i]);
+			}
+
+			$(".robotMarker__imageFader").css("opacity","0");
+			$(".robotMarker__markerCircle").css("opacity", "0");
 		}
 	});
 
 
 	// Ultrasonic sensor
-	$(".sumoRobot__marker--IRSensor .sumoRobot__markerCircle").on("mouseenter", function(){
-		$(".sumoRobot__marker--IRSensor .sumoRobot__markerCircle").css("opacity", "1");
-		$(".sumoRobot__marker--IRSensor .sumoRobot__markerLine").css("opacity", "1");
-		$(".sumoRobot__marker--IRSensor .sumoRobot__markerDescription").css("opacity", "1");
+	$(".robotMarker__marker--IRSensor .robotMarker__markerCircle").on("mouseenter", function(){
+		$(".robotMarker__marker--IRSensor .robotMarker__markerCircle").css("opacity", "1");
+		$(".robotMarker__marker--IRSensor .robotMarker__markerLine").css("opacity", "1");
+		$(".robotMarker__marker--IRSensor .robotMarker__markerDescription").css("opacity", "1");
 	});
 
-	$(".sumoRobot__marker--IRSensor .sumoRobot__markerCircle").on("mouseleave", function(){
-		$(".sumoRobot__marker--IRSensor .sumoRobot__markerCircle").css("opacity", "0.7");
-		$(".sumoRobot__marker--IRSensor .sumoRobot__markerLine").css("opacity", "0");
-		$(".sumoRobot__marker--IRSensor .sumoRobot__markerDescription").css("opacity", "0");
+	$(".robotMarker__marker--IRSensor .robotMarker__markerCircle").on("mouseleave", function(){
+		$(".robotMarker__marker--IRSensor .robotMarker__markerCircle").css("opacity", "0.7");
+		$(".robotMarker__marker--IRSensor .robotMarker__markerLine").css("opacity", "0");
+		$(".robotMarker__marker--IRSensor .robotMarker__markerDescription").css("opacity", "0");
 	});
 
 	// Aluminium Metal Chassis
-	$(".sumoRobot__marker--chassis .sumoRobot__markerCircle").on("mouseenter", function(){
-		$(".sumoRobot__marker--chassis .sumoRobot__markerCircle").css("opacity", "1");
-		$(".sumoRobot__marker--chassis .sumoRobot__markerLine").css("opacity", "1");
-		$(".sumoRobot__marker--chassis .sumoRobot__markerDescription").css("opacity", "1");
+	$(".robotMarker__marker--chassis .robotMarker__markerCircle").on("mouseenter", function(){
+		$(".robotMarker__marker--chassis .robotMarker__markerCircle").css("opacity", "1");
+		$(".robotMarker__marker--chassis .robotMarker__markerLine").css("opacity", "1");
+		$(".robotMarker__marker--chassis .robotMarker__markerDescription").css("opacity", "1");
 	});
 
-	$(".sumoRobot__marker--chassis .sumoRobot__markerCircle").on("mouseleave", function(){
-		$(".sumoRobot__marker--chassis .sumoRobot__markerCircle").css("opacity", "0.7");
-		$(".sumoRobot__marker--chassis .sumoRobot__markerLine").css("opacity", "0");
-		$(".sumoRobot__marker--chassis .sumoRobot__markerDescription").css("opacity", "0");
+	$(".robotMarker__marker--chassis .robotMarker__markerCircle").on("mouseleave", function(){
+		$(".robotMarker__marker--chassis .robotMarker__markerCircle").css("opacity", "0.7");
+		$(".robotMarker__marker--chassis .robotMarker__markerLine").css("opacity", "0");
+		$(".robotMarker__marker--chassis .robotMarker__markerDescription").css("opacity", "0");
 	});
 
 	// Dual Motors
-	$(".sumoRobot__marker--motors .sumoRobot__markerCircle").on("mouseenter", function(){
-		$(".sumoRobot__marker--motors .sumoRobot__markerCircle").css("opacity", "1");
-		$(".sumoRobot__marker--motors .sumoRobot__markerLine").css("opacity", "1");
-		$(".sumoRobot__marker--motors .sumoRobot__markerDescription").css("opacity", "1");
+	$(".robotMarker__marker--motors .robotMarker__markerCircle").on("mouseenter", function(){
+		$(".robotMarker__marker--motors .robotMarker__markerCircle").css("opacity", "1");
+		$(".robotMarker__marker--motors .robotMarker__markerLine").css("opacity", "1");
+		$(".robotMarker__marker--motors .robotMarker__markerDescription").css("opacity", "1");
 	});
 
-	$(".sumoRobot__marker--motors .sumoRobot__markerCircle").on("mouseleave", function(){
-		$(".sumoRobot__marker--motors .sumoRobot__markerCircle").css("opacity", "0.7");
-		$(".sumoRobot__marker--motors .sumoRobot__markerLine").css("opacity", "0");
-		$(".sumoRobot__marker--motors .sumoRobot__markerDescription").css("opacity", "0");
+	$(".robotMarker__marker--motors .robotMarker__markerCircle").on("mouseleave", function(){
+		$(".robotMarker__marker--motors .robotMarker__markerCircle").css("opacity", "0.7");
+		$(".robotMarker__marker--motors .robotMarker__markerLine").css("opacity", "0");
+		$(".robotMarker__marker--motors .robotMarker__markerDescription").css("opacity", "0");
 	});
 
 });
